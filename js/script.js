8c92a372184345698a00161f03e3c7cb808a7ba1
abdullahalahmed <abdullah.s.alahmed01@gmail.com>
2026-08-26 01:11:06 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function validateForm() {
+
+alert("Account Created Successfully!");
+
+return true;
+}
+
+function login() {
+    alert("Login Successful!");
+    document.getElementById("bookBtn").style.display = "block";
+    return false;
+}
